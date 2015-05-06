214a7618a316b4916bb2a286270cad4aae771d1e
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-05-06 17:46:17 +0900

    add CardView sample

--- app/src/main/java/xyz/yyagi/sampleapp/services/TravelBaseService.java
+++ app/src/main/java/xyz/yyagi/sampleapp/services/TravelBaseService.java
@@ -4,8 +4,10 @@ import com.orhanobut.wasp.CallBack;
 import com.orhanobut.wasp.http.BodyMap;
 import com.orhanobut.wasp.http.GET;
 import com.orhanobut.wasp.http.Header;
+import com.orhanobut.wasp.http.Mock;
 import com.orhanobut.wasp.http.POST;
 import com.orhanobut.wasp.http.Path;
+import com.orhanobut.wasp.http.RetryPolicy;
 
 import java.util.List;
 import java.util.Map;
@@ -17,6 +19,8 @@ import xyz.yyagi.sampleapp.models.Travel;
  * Created by yaginuma on 15/05/05.
  */
 public interface TravelBaseService {
+    @Mock
+    @RetryPolicy(timeout = 5000)
     @POST("/oauth/token")
     void authenticate(
         @Header("Authorization") String authToken,
@@ -24,6 +28,7 @@ public interface TravelBaseService {
         CallBack<Authorization> callBack
     );
 
+    @Mock
     @GET("/api/{version}/travels")
     void fetchTravels(
         @Header("Authorization") String authToken,
